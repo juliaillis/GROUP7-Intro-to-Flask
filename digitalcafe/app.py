f31dcfa666a656ad0eae0b5e5578216607806aed
Nathanielle Trina B. Hombre <113453565+3nahombre@users.noreply.github.com>
2023-04-12 23:41:10 +0800

    Modified

--- digitalcafe/app.py
+++ digitalcafe/app.py
@@ -1,9 +1,18 @@
-from flask import Flask
+from flask import Flask,redirect
 from flask import render_template
 from flask import request
 import database as db
+import authentication
+import logging
 
 app = Flask(__name__)
+
+# Set the secret key to some random bytes. 
+# Keep this really secret!
+app.secret_key = b's@g@d@c0ff33!'
+
+logging.basicConfig(level=logging.DEBUG)
+app.logger.setLevel(logging.INFO)
 
 @app.route('/')
 def index():
@@ -34,3 +43,51 @@ def branchdetails():
 @app.route('/aboutus')
 def aboutus():
     return render_template('aboutus.html', page="About Us")
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    return render_template('login.html')
+
+@app.route('/auth', methods = ['GET', 'POST'])
+def auth():
+    username = request.form.get('username')
+    password = request.form.get('password')
+
+    is_successful, user = authentication.login(username, password)
+    app.logger.info('%s', is_successful)
+    if(is_successful):
+        session["user"] = user
+        return redirect('/')
+    else:
+        return redirect('/login')
+
+@app.route('/logout')
+def logout():
+    session.pop("user",None)
+    session.pop("cart",None)
+    return redirect('/')
+
+@app.route('/addtocart')
+def addtocart():
+    code = request.args.get('code', '')
+    product = db.get_product(int(code))
+    item=dict()
+    # A click to add a product translates to a 
+    # quantity of 1 for now
+
+    item["qty"] = 1
+    item["name"] = product["name"]
+    item["subtotal"] = product["price"]*item["qty"]
+
+    if(session.get("cart") is None):
+        session["cart"]={}
+
+    cart = session["cart"]
+    cart[code]=item
+    session["cart"]=cart
+    return redirect('/cart')
+
+@app.route('/cart')
+def cart():
+    return render_template('cart.html')
+
--- digitalcafe/database.py
+++ digitalcafe/database.py
@@ -16,6 +16,30 @@ branches = {
     5: {"name":"Arcovia","phonenumber":"09179990004"},
 }
 
+users = {
+    "chums@example.com":{"password":"Ch@ng3m3!",
+                         "first_name":"Matthew",
+                         "last_name":"Uy"},
+    "joben@example.com":{"password":"Ch@ng3m3!",
+                         "first_name":"Joben",
+                         "last_name":"Ilagan"},
+    "bong@example.com":{"password":"Ch@ng3m3!",
+                        "first_name":"Bong",
+                        "last_name":"Olpoc"},
+    "joaqs@example.com":{"password":"Ch@ng3m3!",
+                         "first_name":"Joaqs",
+                         "last_name":"Gonzales"},
+    "gihoe@example.com":{"password":"Ch@ng3m3!",
+                         "first_name":"Gio",
+                         "last_name":"Hernandez"},
+    "vic@example.com":{"password":"Ch@ng3m3!",
+                       "first_name":"Vic",
+                       "last_name":"Reventar"},
+    "joe@example.com":{"password":"Ch@ng3m3!",
+                       "first_name":"Joe",
+                       "last_name":"Ilagan"},
+}
+
 def get_product(code):
     return products[code]
 
@@ -42,3 +66,8 @@ def get_branches():
 
     return branch_list
 
+def get_user(username):
+    try:
+       return users[username]
+    except KeyError:
+       return None
