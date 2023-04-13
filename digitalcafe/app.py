e758ef0c55c69ef8df6b68ce4ff44989fce9d2e5
juliaillis <122949974+juliaillis@users.noreply.github.com>
2023-04-13 08:15:25 +0800

    Update app.py

--- digitalcafe/app.py
+++ digitalcafe/app.py
@@ -1,6 +1,7 @@
 from flask import Flask,redirect
 from flask import render_template
 from flask import request
+from flask import session
 import database as db
 import authentication
 import logging
